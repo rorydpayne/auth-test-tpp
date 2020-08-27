01273b00b229b17141c8ab9b97f7e3e7dd15cc01
Rory Payne <rory.payne@zopa.com>
2020-08-27 17:34:27 +0100

    Updates

---
+++ precache-manifest.d8a711eb659b80f839d81aceabe2a342.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ad55ce06f42ede60631e867e08b645e",
+    "url": "/auth-test-tpp/index.html"
+  },
+  {
+    "revision": "d4d4412afe6d5a3ea70c",
+    "url": "/auth-test-tpp/static/js/2.310b51c1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/auth-test-tpp/static/js/2.310b51c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c95e129d106bc663b745",
+    "url": "/auth-test-tpp/static/js/main.e91ea013.chunk.js"
+  },
+  {
+    "revision": "a9b4808547e890c74465",
+    "url": "/auth-test-tpp/static/js/runtime-main.3c0b3da3.js"
+  }
+]);
